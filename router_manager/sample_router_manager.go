73283ff75d7de649a65c122cfe3c4bcd13878c42
faisal.porag <faisal.porag@technonext.com>
2023-11-22 11:44:55 +0600

    params added

--- router_manager/sample_router_manager.go
+++ router_manager/sample_router_manager.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 // MountCustomerRoutes mounts the "/customer" routes
 func MountCustomerRoutes(router *gin.RouterGroup) {
 	router.GET("/", homeHandler)
+	router.POST("/signup", createCustomer)
 	router.POST("/sign-in", customerSignInHandler)
 	router.GET("/profile", customerProfileHandler)
 }
@@ -44,4 +45,41 @@ func customerProfileHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Customer profile endpoint",
 	})
+}
+
+// @Summary Create a new customer.
+// @Description Create a new customer.
+// @ID create-customer
+// @Accept json
+// @Produce json
+// @Param input body CustomerInput true "Customer information to create"
+// @Success 201 {object} CustomerResponse
+// @Router /customer/signup [post]
+func createCustomer(c *gin.Context) {
+	var input CustomerInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Your logic to create a new customer with the provided input
+	// ...
+
+	// Return the created customer
+	c.JSON(201, CustomerResponse{
+		ID:   3, // Replace with the actual ID of the created customer
+		Name: input.Name,
+		// Include other fields as needed
+	})
+}
+
+type CustomerInput struct {
+	Name string `json:"name" binding:"required"`
+	// Add other input fields as needed
+}
+
+type CustomerResponse struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	// Add other response fields as needed
 }
